93229d811cd3345566947e1298abfb2d60fa3891
expertman <javaexpert@nate.com>
2022-08-03 10:33:32 +0900

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("성적관리프로그램 시작");
+		Student jimin = new Student();
+		Input input = new Input();       input.input(jimin);
+		Calc  calc = new Calc();        calc.calc(jimin);
+		Output output = new Output();   output.output(jimin);
+		System.out.println("성적관리프록램 끝");
+	}
+}
